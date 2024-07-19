74b90d6e03ef8d343b9befe9b6571a9e29b268bf
Aashish <94863305+Aashish-Sisotiya@users.noreply.github.com>
2024-07-20 02:15:44 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://real-estate-app-backend-xfbq.onrender.com',
         changeOrigin: true,
       },
     },
